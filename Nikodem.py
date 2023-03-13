4675c6e2ac3636acd99a4405b86e51ea9b1778d4
NikodemDudek <120924896+NikodemDudek@users.noreply.github.com>
2023-03-13 16:21:33 +0100

    Update Nikodem.py

--- Nikodem.py
+++ Nikodem.py
@@ -3,7 +3,4 @@ import handle_data
 if __name__ == '__main__':
     # handle_data.folders_to_csv("logfiles", "logfiles.csv")
     data = handle_data.csv_to_dataframe("logfiles.csv")
-
     data = handle_data.manipulate_data(data)
-    data_avg = handle_data.average_data(data)
-    handle_data.manual_check_data(data)
