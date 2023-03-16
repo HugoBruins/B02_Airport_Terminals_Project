5d72b88006630247d3539326952ec0208e241741
gaiaejlal <gaia.ejlal@gmail.com>
2023-03-16 11:14:19 +0100

    corr

--- corelation_matrix.py
+++ corelation_matrix.py
@@ -1,6 +1,8 @@
 import plotly.express as px
 from numpy import *
 import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
 
 #pulling and reading the file
 logfile= pd.read_csv('logfiles_averaged_scenarios.csv')
@@ -9,4 +11,4 @@ print(df)
 
 corr_matrix = df.corr()
 sn.heatmap(corr_matrix, annot=True)
-plt.show()c
+plt.show()
