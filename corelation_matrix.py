accbdcda2d7e5259b78bbadbade8ea859face9ab
FreyavanA <freyavanapelhoorn@gmail.com>
2023-03-30 09:21:37 +0200

    Fixed some aestetic things

--- corelation_matrix.py
+++ corelation_matrix.py
@@ -32,19 +32,19 @@ corr_matrix = corr_matrix.drop(column_names[0: 19], axis = 1)
 corr_matrix = corr_matrix.drop(column_names[19:], axis = 0)
 
 print(corr_matrix)
-sn.heatmap(corr_matrix, annot=True, cmap="Reds", linewidth=.5)
+sn.heatmap(corr_matrix, annot=True, cmap="Reds", linewidth=.5, vmin=-1, vmax=1)
 
 #naming the ticks appropriately 
 # plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],[" ", "Avg. Que TimeCl", "Avg. Que TimeSC", "Avg. Time to Gate", "Pax. Check-In Compl.SC", "Pax. Check-In Compl.CL", "No. Missed Flights", "Total Expenditure", "Avg. Max Pax in QueCL", "Max Pax. in QueSC", "Check-In Strat.", "Security Checkpoint Strat.", "Call to Gate Strat.", "Identifier Scenario", "Identifier Run", "Tot. No. Pax Missed Flights"], rotation=45, ha='right', minor=False)
 #plt.yticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],[" ", "Avg. Que TimeCl", "Avg. Que TimeSC", "Avg. Time to Gate", "Pax. Check-In Compl.SC", "Pax. Check-In Compl.CL", "No. Missed Flights", "Total Expenditure", "Avg. Max Pax in QueCL", "Max Pax. in QueSC", "Check-In Strat.", "Security Checkpoint Strat.", "Call to Gate Strat.", "Identifier Scenario", "Identifier Run", "Tot. No. Pax Missed Flights"])
 #plt.xticks([1,2,3,4,5,6,7,8,9], ["Avg. Que TimeCl", "Avg. Que TimeSC", "Avg. Time to Gate", "Pax. Check-In Compl.SC", "Pax. Check-In Compl.CL", "No. Missed Flights", "Total Expenditure", "Avg. Max Pax in QueCL", "Max Pax. in QueSC"], rotation=45, ha='right', minor=False)
 
-abbrev_inputs = ["TotPass", "CallToGateStrat", "NPax1", "NPax2", "NPax3", "NPax4", "NPax5", "NPax6", "NPax7", "C7200-6300", "C6300-5400", "C5400-4500", "C4500-3600", "C3600-2700", "S<1800", "S1800-3600", "S7200-9000", "S9000-10800", "S>10800"]
+abbrev_inputs = ["TotPax", "CallToGateStrat", "NPax1", "NPax2", "NPax3", "NPax4", "NPax5", "NPax6", "NPax7", "C7200-6300", "C6300-5400", "C5400-4500", "C4500-3600", "C3600-2700", "S<1800", "S1800-3600", "S7200-9000", "S9000-10800", "S>10800"]
 abbrev_outputs = ["Avg. Que TimeCl", "Avg. Que TimeSC", "Avg. Time to Gate", "Pax. Check-In Compl.SC", "Pax. Check-In Compl.CL", "No. Missed Flights", "Total Expenditure", "Avg. Max Pax in QueCL", "Max Pax. in QueSC"]
 plt.xticks([1,2,3,4,5,6,7,8,9], abbrev_outputs, rotation=45, ha='right', minor=False)
 plt.yticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19], abbrev_inputs)
 
 
 #plotting the graph
-plt.title('Correlation Matrix', fontsize=16)
+# plt.title('Correlation Matrix', fontsize=16)
 plt.show()
